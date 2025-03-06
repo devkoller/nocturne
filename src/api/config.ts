f71e99ea719a02a78cbfefc20e331b5e22e1a416
Julio Mariscal <devkoller.jm@gmail.com>
2025-03-05 23:28:39 -0600

    delete a /

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,4 +2,4 @@ export const PORT = window.location.protocol === "http:" ? ":3010" : ":3011"
 
 export const API_URL = import.meta.env.DEV
 	? `http://${window.location.hostname}${PORT}`
-	: "https://medica.meditiva.com.mx/"
+	: "https://medica.meditiva.com.mx"
